e925c8e5d02e40a964374d2b11e26e7535f08881
Yamashita Yuu <peek824545201@gmail.com>
2014-12-27 17:12:58 +0900

    Read version info from `tdclient.__version__`

--- setup.py
+++ setup.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python
 
 from __future__ import with_statement
-
 from setuptools import setup, find_packages
+import re
+
+version = "0.0.1.dev.0"
+with open("tdclient/version.py") as fp:
+    m = re.search(r"^__version__ *= *\"([^\"]*)\" *$", fp.read(), re.MULTILINE)
+    if m is not None:
+        version = m.group(1)
 
 setup(
-  name="td-client",
-  version="0.0.1",
-  description="Treasure Data API library for Python",
-  author="Treasure Data, Inc.",
-  author_email="support@treasure-data.com",
-  url="http://treasuredata.com/",
-  install_requires=[
-    "msgpack-python>=0.4,<0.5",
-    "pytest>=2.6,<2.7.0",
-    "tox>=1.8,<1.9.0",
-  ],
-  packages=find_packages(),
-  test_suite="tdclient.test",
-  license="Apache Software License",
-  platforms="Posix; MacOS X; Windows",
-  classifiers=[
-    "Development Status :: 3 - Alpha",
-    "Environment :: Web Environment",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: Apache Software License",
-    "Operating System :: OS Independent",
-    "Topic :: Internet"
-  ],
+    name="td-client",
+    version=version,
+    description="Treasure Data API library for Python",
+    author="Treasure Data, Inc.",
+    author_email="support@treasure-data.com",
+    url="http://treasuredata.com/",
+    install_requires=[
+        "msgpack-python>=0.4,<0.5",
+        "pytest>=2.6,<2.7.0",
+        "tox>=1.8,<1.9.0",
+    ],
+    packages=find_packages(),
+    test_suite="tdclient.test",
+    license="Apache Software License",
+    platforms="Posix; MacOS X; Windows",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+    ],
 )
--- tdclient/__init__.py
+++ tdclient/__init__.py
@@ -1,2 +1,3 @@
 from tdclient.client import *
+from tdclient.version import __version__
 __all__ = ["Client"]
