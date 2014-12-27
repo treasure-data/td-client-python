07b1602fbab9708929ac331617f4d6635b6e503d
Yamashita Yuu <peek824545201@gmail.com>
2014-12-27 20:08:18 +0900

    Add test for `/v3/job/list`

---
+++ tdclient/test/job_api_test.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import with_statement
+
+import functools
+import os
+
+from tdclient import api
+from tdclient import version
+
+def setup_function(function):
+    try:
+        del os.environ["TD_API_SERVER"]
+    except KeyError:
+        pass
+    try:
+        del os.environ["HTTP_PROXY"]
+    except KeyError:
+        pass
+
+class Response(object):
+    def __init__(self, status, body, headers):
+        self.status = status
+        self.body = body.encode("utf-8")
+        self.headers = headers
+        self.request_method = None
+        self.request_path = None
+        self.request_headers = None
+
+def get(response, url, params={}):
+    response.request_method = "GET"
+    response.request_path = url
+    response.request_headers = params
+    return (response.status, response.body, response)
+
+def test_list_jobs():
+    client = api.API("apikey")
+    body = """
+        {
+            "count":11,"from":0,"to":10,"jobs":[
+                {"status":"success","cpu_time":496570,"result_size":24,"duration":262,"job_id":"18882028","created_at":"2014-12-23 12:00:18 UTC","updated_at":"2014-12-23 12:04:42 UTC","start_at":"2014-12-23 12:00:19 UTC","end_at":"2014-12-23 12:04:41 UTC","query":"select count(1) from import_test","type":"hive","priority":0,"retry_limit":0,"result":"","url":"http://console.treasuredata.com/jobs/18882028","user_name":"owner","hive_result_schema":"[[\\"_c0\\", \\"bigint\\"]]","organization":null,"database":"jma_weather"},
+                {"status":"success","cpu_time":489540,"result_size":24,"duration":272,"job_id":"18880612","created_at":"2014-12-23 11:00:16 UTC","updated_at":"2014-12-23 11:04:48 UTC","start_at":"2014-12-23 11:00:16 UTC","end_at":"2014-12-23 11:04:48 UTC","query":"select count(1) from import_test","type":"hive","priority":0,"retry_limit":0,"result":"","url":"http://console.treasuredata.com/jobs/18880612","user_name":"owner","hive_result_schema":"[[\\"_c0\\", \\"bigint\\"]]","organization":null,"database":"jma_weather"},
+                {"status":"success","cpu_time":486630,"result_size":24,"duration":263,"job_id":"18879199","created_at":"2014-12-23 10:00:21 UTC","updated_at":"2014-12-23 10:04:44 UTC","start_at":"2014-12-23 10:00:21 UTC","end_at":"2014-12-23 10:04:44 UTC","query":"select count(1) from import_test","type":"hive","priority":0,"retry_limit":0,"result":"","url":"http://console.treasuredata.com/jobs/18879199","user_name":"owner","hive_result_schema":"[[\\"_c0\\", \\"bigint\\"]]","organization":null,"database":"jma_weather"}
+            ]
+        }
+    """
+    response = Response(200, body, {})
+    client.get = functools.partial(get, response)
+    jobs = client.list_jobs(0, 2)
+    assert response.request_method == "GET"
+    assert response.request_path == "/v3/job/list"
+    assert len(jobs) == 3
+    assert sorted([ job[0] for job in jobs ]) == ["18879199", "18880612", "18882028"]
