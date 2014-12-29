0eb98395fac95999b92918d96239cc575f4e8e53
Yamashita Yuu <peek824545201@gmail.com>
2014-12-29 11:39:40 +0900

    Setup the integration with setuptools test commands
    http://pytest.org/latest/goodpractises.html

--- setup.py
+++ setup.py
@@ -2,13 +2,32 @@
 
 from __future__ import with_statement
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 import re
+import sys
 
 version = "0.0.1.dev.0"
 with open("tdclient/version.py") as fp:
     m = re.search(r"^__version__ *= *\"([^\"]*)\" *$", fp.read(), re.MULTILINE)
     if m is not None:
         version = m.group(1)
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name="td-client",
@@ -18,13 +37,16 @@ setup(
     author_email="support@treasure-data.com",
     url="http://treasuredata.com/",
     install_requires=[
-        "coveralls>=0.5,<0.6",
         "msgpack-python>=0.4,<0.5",
+    ],
+    tests_require=[
+        "coveralls>=0.5,<0.6",
         "pytest>=2.6,<2.7",
         "pytest-cov>=1.8,<1.9",
         "tox>=1.8,<1.9",
     ],
     packages=find_packages(),
+    cmdclass = {"test": PyTest},
     test_suite="tdclient.test",
     license="Apache Software License",
     platforms="Posix; MacOS X; Windows",
