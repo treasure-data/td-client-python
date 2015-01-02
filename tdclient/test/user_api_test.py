b165e1ded6e8152803eb5f4b6ec231a7b479cd73
Yamashita Yuu <peek824545201@gmail.com>
2015-01-03 01:20:06 +0900

    Fix `coveralls` on travis-ci

--- tdclient/test/user_api_test.py
+++ tdclient/test/user_api_test.py
@@ -5,9 +5,9 @@ from __future__ import unicode_literals
 from __future__ import with_statement
 
 try:
-    import mock
-except ImportError:
     from unittest import mock
+except ImportError:
+    import mock
 import pytest
 
 from tdclient import user_api
