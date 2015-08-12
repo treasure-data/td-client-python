f917fbbcf2193ad50c628f2562895a94063dca06
Yamashita, Yuu <peek824545201@gmail.com>
2015-08-12 19:21:56 +0900

    add data connector APIs

---
+++ tdclient/bulk_load_api.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import json
+try:
+    from urllib.parse import quote as urlquote # >=3.0
+except ImportError:
+    from urllib import quote as urlquote
+
+class BulkLoadAPI(object):
+    ####
+    ## Bulk load API
+    ##
+
+    def bulk_load_guess(self, job):
+        """
+        TODO: add docstring
+        """
+        with self.post("/v3/bulk_loads/guess", json.dumps(job)):
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("BulkLoad configuration guess failed", res, body)
+            return json.loads(body)
+
+    def bulk_load_preview(self, job):
+        """
+        TODO: add docstring
+        """
+        params = {} if params is None else params
+        with self.post("/v3/bulk_loads/preview", json.dumps(job)):
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("BulkLoad job preview failed", res, body)
+            return json.loads(body)
+
+    def bulk_load_issue(self, database, table, job):
+        """
+        TODO: add docstring
+        """
+        params = dict(job)
+        params["database"] = database
+        params["table"] = table
+        with self.post("/v3/job/issue/bulkload/%s" % (urlquote(str(db))), params) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("BulkLoad job issuing failed", res, body)
+            js = self.checked_json(body, ["job_id"])
+            return str(js["job_id"])
+
+    def bulk_load_list(self):
+        """
+        TODO: add docstring
+        """
+        with self.get("/v3/bulk_loads"):
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("BulkLoadSession list retrieve failed", res, body)
+            return json.loads(body)
+
+    def bulk_load_create(self, name, database, table, job, params=None):
+        """
+        TODO: add docstring
+        """
+        params = {} if opts is None else dict(params)
+        params.update(job)
+        params["name"] = name
+        params["database"] = database
+        params["table"] = table
+        with self.post("/v3/bulk_loads", json.dumps(job)):
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("BulkLoadSession: %s created failed" % (name,), res, body)
+            return json.loads(body)
+
+    def bulk_load_show(self, name):
+        """
+        TODO: add docstring
+        """
+        with self.get("/v3/bulk_loads/%s" % (urlquote(str(name)),)):
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("BulkLoadSession: %s retrieve failed" % (name,), res, body)
+            return json.loads(body)
+
+    def bulk_load_update(self, name, job):
+        """
+        TODO: add docstring
+        """
+        with self.put("/v3/bulk_loads/%s" % (urlquote(str(name)),)):
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("BulkLoadSession: %s update failed" % (name,), res, body)
+            return json.loads(body)
+
+    def bulk_load_delete(self, name):
+        """
+        TODO: add docstring
+        """
+        with self.delete("/v3/bulk_loads/%s" % (urlquote(str(name)),)):
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("BulkLoadSession: %s delete failed" % (name,), res, body)
+            return json.loads(body)
+
+    def bulk_load_history(self, name):
+        """
+        TODO: add docstring
+        """
+        with self.get("/v3/bulk_loads/%s" % (urlquote(str(name)),)):
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("history of BulkLoadSession: %s retrieve failed" % (name,), res, body)
+            return json.loads(body)
+
+    def bulk_load_run(self, name, params=None):
+        """
+        TODO: add docstring
+        """
+        params = {} if params is None else params
+        with self.post("/v3/bulk_loads/%s" % (urlquote(str(name)),), params):
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("BulkLoadSession: %s job create failed" % (name,), res, body)
+            return json.loads(body)
