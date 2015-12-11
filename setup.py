f3383082c37932902e82e9d96a48b94a2fa3e4ac
Yamashita, Yuu <peek824545201@gmail.com>
2015-12-11 09:04:37 +0000

    Add a workaround for `AttributeError` from `test_args` in `setup.py`
    https://github.com/pytest-dev/pytest/issues/1134

--- setup.py
+++ setup.py
@@ -22,11 +22,6 @@ class PyTest(TestCommand):
         TestCommand.initialize_options(self)
         self.pytest_args = []
 
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
     def run_tests(self):
         import pytest
         errno = pytest.main(self.pytest_args)
