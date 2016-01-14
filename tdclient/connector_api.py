4fc2d27539b2c889d1ee79a6c57ff999512e4ce2
Yamashita, Yuu <peek824545201@gmail.com>
2016-01-14 09:00:53 +0000

    s/BulkLoad/DataConnector/g

--- tdclient/connector_api.py
+++ tdclient/connector_api.py
@@ -21,7 +21,7 @@ class ConnectorAPI(object):
         with self.post("/v3/bulk_loads/guess", job) as res:
             code, body = res.status, res.read()
             if code != 200:
-                self.raise_error("BulkLoad configuration guess failed", res, body)
+                self.raise_error("DataConnector configuration guess failed", res, body)
             return self.checked_json(body, [])
 
     def connector_preview(self, job):
@@ -31,7 +31,7 @@ class ConnectorAPI(object):
         with self.post("/v3/bulk_loads/preview", job) as res:
             code, body = res.status, res.read()
             if code != 200:
-                self.raise_error("BulkLoad job preview failed", res, body)
+                self.raise_error("DataConnector job preview failed", res, body)
             return self.checked_json(body, [])
 
     def connector_issue(self, db, table, job):
@@ -44,7 +44,7 @@ class ConnectorAPI(object):
         with self.post("/v3/job/issue/bulkload/%s" % (urlquote(str(db))), params) as res:
             code, body = res.status, res.read()
             if code != 200:
-                self.raise_error("BulkLoad job issuing failed", res, body)
+                self.raise_error("DataConnector job issuing failed", res, body)
             js = self.checked_json(body, ["job_id"])
             return str(js["job_id"])
 
@@ -55,7 +55,7 @@ class ConnectorAPI(object):
         with self.get("/v3/bulk_loads") as res:
             code, body = res.status, res.read()
             if code != 200:
-                self.raise_error("BulkLoadSession list retrieve failed", res, body)
+                self.raise_error("DataConnectorSession list retrieve failed", res, body)
             # cannot use `checked_json` since `GET /v3/bulk_loads` returns an array
             return json.loads(body.decode("utf-8"))
 
@@ -71,7 +71,7 @@ class ConnectorAPI(object):
         with self.post("/v3/bulk_loads", params) as res:
             code, body = res.status, res.read()
             if code != 200:
-                self.raise_error("BulkLoadSession: %s created failed" % (name,), res, body)
+                self.raise_error("DataConnectorSession: %s created failed" % (name,), res, body)
             return self.checked_json(body, [])
 
     def connector_show(self, name):
@@ -81,7 +81,7 @@ class ConnectorAPI(object):
         with self.get("/v3/bulk_loads/%s" % (urlquote(str(name)),)) as res:
             code, body = res.status, res.read()
             if code != 200:
-                self.raise_error("BulkLoadSession: %s retrieve failed" % (name,), res, body)
+                self.raise_error("DataConnectorSession: %s retrieve failed" % (name,), res, body)
             return self.checked_json(body, [])
 
     def connector_update(self, name, job):
@@ -91,7 +91,7 @@ class ConnectorAPI(object):
         with self.put("/v3/bulk_loads/%s" % (urlquote(str(name)),), job) as res:
             code, body = res.status, res.read()
             if code != 200:
-                self.raise_error("BulkLoadSession: %s update failed" % (name,), res, body)
+                self.raise_error("DataConnectorSession: %s update failed" % (name,), res, body)
             return self.checked_json(body, [])
 
     def connector_delete(self, name):
@@ -101,7 +101,7 @@ class ConnectorAPI(object):
         with self.delete("/v3/bulk_loads/%s" % (urlquote(str(name)),)) as res:
             code, body = res.status, res.read()
             if code != 200:
-                self.raise_error("BulkLoadSession: %s delete failed" % (name,), res, body)
+                self.raise_error("DataConnectorSession: %s delete failed" % (name,), res, body)
             return self.checked_json(body, [])
 
     def connector_history(self, name):
@@ -111,7 +111,7 @@ class ConnectorAPI(object):
         with self.get("/v3/bulk_loads/%s" % (urlquote(str(name)),)) as res:
             code, body = res.status, res.read()
             if code != 200:
-                self.raise_error("history of BulkLoadSession: %s retrieve failed" % (name,), res, body)
+                self.raise_error("history of DataConnectorSession: %s retrieve failed" % (name,), res, body)
             return self.checked_json(body, [])
 
     def connector_run(self, name, **kwargs):
@@ -121,5 +121,5 @@ class ConnectorAPI(object):
         with self.post("/v3/bulk_loads/%s" % (urlquote(str(name)),), kwargs) as res:
             code, body = res.status, res.read()
             if code != 200:
-                self.raise_error("BulkLoadSession: %s job create failed" % (name,), res, body)
+                self.raise_error("DataConnectorSession: %s job create failed" % (name,), res, body)
             return self.checked_json(body, [])
