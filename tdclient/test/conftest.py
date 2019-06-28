7e2d7e3e4c8017ce6ad70d56d74939ebabb38bec
Atsushi Odagiri <aodagx@gmail.com>
2019-06-28 16:59:11 +0900

    added test matrix switching pyopenssl

---
+++ tdclient/test/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+try:
+    from urllib3.contrib.pyopenssl import inject_into_urllib3, extract_from_urllib3
+except ImportError:
+    inject_into_urllib3 = lambda: None
+    extract_from_urllib3 = lambda: None
+
+
+@pytest.fixture(autouse=True)
+def pyopenssl_inject_into_urllib3():
+    inject_into_urllib3()
+    try:
+        yield
+    finally:
+        extract_from_urllib3()
