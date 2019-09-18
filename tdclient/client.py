fdc36a399da3879d28c8f6c01fea2e59a9592ed6
Aki Ariga <ariga@treasure-data.com>
2019-09-18 18:41:42 +0900

    doc: add docstring for Client.partial_delete

--- tdclient/client.py
+++ tdclient/client.py
@@ -419,9 +419,27 @@ class Client:
         return models.Job(self, job_id, "export", None)
 
     def partial_delete(self, db_name, table_name, to, _from, params=None):
-        """
-        TODO: add docstring
-        => :class:`tdclient.models.Job`
+        """Create a job to partially delete the contents of the table with the given
+        time range.
+
+        Args:
+            db (str): Target database name.
+            table (str): Target table name.
+            to (int): Time in Unix Epoch format indicating the End date and time of the
+                data to be deleted. Should be set only by the hour. Minutes and seconds
+                values will not be accepted.
+            _from (int): Time in Unix Epoch format indicating the Start date and time of
+                the data to be deleted. Should be set only by the hour. Minutes and
+                seconds values will not be accepted.
+            params (dict, optional): Extra parameters.
+                pool_name (str, optional): Indicates the resource pool to execute this
+                    job. If not provided, the account's default resource pool would be
+                    used.
+                domain_key (str, optional): Domain key that will be assigned to the
+                    partial delete job to be created
+
+        Returns:
+             :class:`tdclient.models.Job`
         """
         params = {} if params is None else params
         job_id = self.api.partial_delete(db_name, table_name, to, _from, params)
