4b7d68738c9c21cfe516ac012d1c7990730f9b7c
Aki Ariga <chezou+github@gmail.com>
2019-09-26 17:36:12 +0900

    fix: change appropreate file for long description

--- setup.py
+++ setup.py
@@ -6,9 +6,11 @@ import os
 import re
 import sys
 
+
 def read(fname):
     with open(os.path.join(os.path.dirname(__file__), fname)) as fp:
         return fp.read()
+
 
 m = re.search(r"^__version__ *= *\"([^\"]*)\" *$", read("tdclient/version.py"), re.MULTILINE)
 
@@ -16,6 +18,7 @@ if m is None:
     raise(RuntimeError("could not read tdclient/version.py"))
 else:
     version = m.group(1)
+
 
 class PyTest(TestCommand):
     user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
@@ -29,12 +32,13 @@ class PyTest(TestCommand):
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
+
 setup(
     name="td-client",
     version=version,
     description="Treasure Data API library for Python",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown; charset=UTF-8; variant=GFM",
+    long_description=read("README.rst"),
+    long_description_content_type="text/x-rst; charset=UTF-8;",
     author="Treasure Data, Inc.",
     author_email="support@treasure-data.com",
     url="http://treasuredata.com/",
