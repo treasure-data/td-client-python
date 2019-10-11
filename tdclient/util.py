5e499440a958528689a0a5c8c53c40013e29a8a9
Atsushi Odagiri <aodagx@gmail.com>
2019-10-11 15:09:46 +0900

    fix too complexity

--- tdclient/util.py
+++ tdclient/util.py
@@ -10,3 +10,27 @@ def create_url(tmpl, **values):
     """
     quoted_values = {k: urlquote(str(v)) for k, v in values.items()}
     return tmpl.format(**quoted_values)
+
+
+def csv_value(s):
+    """Convert value from csv suitably
+
+    Args:
+        s (str): value on csv
+    Returns:
+        Suitable value (int, float, str, bool or None)
+    """
+    try:
+        return int(s)
+    except (OverflowError, ValueError):
+        try:
+            return float(s)
+        except (OverflowError, ValueError):
+            pass
+    lower = s.lower()
+    if lower in ("false", "true"):
+        return "true" == lower
+    elif lower in ("", "none", "null"):
+        return None
+    else:
+        return s
