1d3807c18d4f74a0520a92803b73143f6ca01b47
Aki Ariga <ariga@treasure-data.com>
2019-12-05 11:11:05 +0900

    doc: remove UserAPI doc

--- tdclient/user_api.py
+++ tdclient/user_api.py
@@ -4,22 +4,7 @@ from .util import create_url
 
 
 class UserAPI:
-    """Access to User API.
-
-    This class is inherited by :class:`tdclient.api.API`.
-    """
-
     def authenticate(self, user, password):
-        """Authenticate the indicated email address which is not authenticated via SSO.
-
-        Args:
-            user (str): Email of the user to be authenticated.
-            password (str): Must contain at least 1 letter, 1 number, and 1 special
-                character such as the following:
-                ``r'[!#$%-_=+<>0-9a-zA-Z]'``
-        Returns:
-            str: API key
-        """
         with self.post(
             "/v3/user/authenticate", {"user": user, "password": password}
         ) as res:
@@ -31,11 +16,6 @@ class UserAPI:
             return apikey
 
     def list_users(self):
-        """Get the list of users for the account.
-
-        Returns:
-            [[name:str,organization:str,[user:str]]
-        """
         with self.get("/v3/user/list") as res:
             code, body = res.status, res.read()
             if code != 200:
@@ -45,16 +25,6 @@ class UserAPI:
             return [user_to_tuple(roleinfo) for roleinfo in js["users"]]
 
     def add_user(self, name, org, email, password):
-        """Add a new user to the current account and sends invitation.
-
-        Args:
-            name (str): User's name
-            org (str): Not used
-            email (str): User's email address
-            password (str): User's temporary password for logging-in
-        Returns:
-            bool: `True` if succeeded.
-        """
         params = {"organization": org, "email": email, "password": password}
         with self.post(create_url("/v3/user/add/{name}", name=name), params) as res:
             code, body = res.status, res.read()
@@ -63,12 +33,6 @@ class UserAPI:
             return True
 
     def remove_user(self, name):
-        """Remove the specified email in the account and revokes the user's access.
-        Args:
-            name (str): User's email address
-        Returns:
-            bool: `True` if succeded
-        """
         with self.post(create_url("/v3/user/remove/{name}", name=name)) as res:
             code, body = res.status, res.read()
             if code != 200:
@@ -76,13 +40,6 @@ class UserAPI:
             return True
 
     def list_apikeys(self, name):
-        """Get the apikeys of the current user.
-
-        Args:
-            name (str): User's email address
-        Returns:
-            [str]: List of API keys
-        """
         with self.get(create_url("/v3/user/apikey/list/{name}", name=name)) as res:
             code, body = res.status, res.read()
             if code != 200:
@@ -91,13 +48,6 @@ class UserAPI:
             return js["apikeys"]
 
     def add_apikey(self, name):
-        """Create a new apikey for the specified email address.
-
-        Args:
-            name (str): User's email address
-        Returns:
-            bool: `True` if succeeded.
-        """
         with self.post(create_url("/v3/user/apikey/add/{name}", name=name)) as res:
             code, body = res.status, res.read()
             if code != 200:
@@ -105,14 +55,6 @@ class UserAPI:
             return True
 
     def remove_apikey(self, name, apikey):
-        """Delete the apikey for the specified email address.
-
-        Args:
-            name (str): User's email address
-            apikey (str): User's apikey to be deleted
-        Returns:
-            bool: `True` if succeeded.
-        """
         params = {"apikey": apikey}
         with self.post(
             create_url("/v3/user/apikey/remove/{name}", name=name), params
